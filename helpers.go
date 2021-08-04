18ff3801ee1f73588ddab946fc0e81c0cd481981
Claudio Netto <nettinhorama@gmail.com>
2021-08-04 06:42:28 -0300

    feat: add helpers funcs to return zero value of a pointers

--- helpers.go
+++ helpers.go
@@ -8,11 +8,43 @@ import "time"
 
 func BoolPointer(b bool) *bool { return &b }
 
+func BoolValue(ptr *bool) bool {
+	if ptr == nil {
+		return false
+	}
+
+	return *ptr
+}
+
 func IntPointer(n int) *int { return &n }
+
+func IntValue(ptr *int) int {
+	if ptr == nil {
+		return 0
+	}
+
+	return *ptr
+}
 
 func StringPointer(s string) *string { return &s }
 
+func StringValue(ptr *string) string {
+	if ptr == nil {
+		return ""
+	}
+
+	return *ptr
+}
+
 func TimePointer(t time.Time) *time.Time { return &t }
+
+func TimeValue(ptr *time.Time) time.Time {
+	if ptr == nil {
+		return time.Time{}
+	}
+
+	return *ptr
+}
 
 func TTL(r Record, d Domain) int {
 	if ttl := r.GetTTL(); ttl != nil {
